a29ea879d4600812e3532e8708db2019af29a5fa
Marcilia <guilgerm@gmail.com>
2020-11-24 21:31:55 -0300

    utilizando a feature traits

---
+++ Features.Tests/01 - Traits/ClienteTests.cs
@@ -0,0 +1,55 @@
+﻿using Features.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            //Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Marcilia",
+                "G.",
+                DateTime.Now.AddYears(-30),
+                "ma@gmail.com",
+                true,
+                DateTime.Now);
+
+            //Act
+            var result = cliente.EhValido();
+
+            //Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+        [Fact(DisplayName = "Novo Cliente inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            //Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "ma@gmail.com",
+                true,
+                DateTime.Now);
+
+            //Act
+            var result = cliente.EhValido();
+
+            //Assert
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+
+    }
+}
