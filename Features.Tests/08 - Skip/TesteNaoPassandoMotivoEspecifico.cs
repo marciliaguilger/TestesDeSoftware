b6f08de60e0d943dfd87a6c00b68389ac0af74bb
Marcilia <guilgerm@gmail.com>
2020-12-09 20:30:37 -0300

    Skip Testes

---
+++ Features.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class TesteNaoPassandoMotivoEspecifico
+    {
+        [Fact(DisplayName = "Novo cliente 2.0", Skip ="Nova Versão 2.0 quebrando")]
+        [Trait("Categoria", "Escapando dos testes")]
+        public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
+        {
+            Assert.True(false);
+        }
+
+    }
+}
