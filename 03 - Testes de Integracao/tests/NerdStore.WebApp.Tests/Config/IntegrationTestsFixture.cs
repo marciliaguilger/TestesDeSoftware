33b44c8f55c7e51b7184ccff6a51d47c1ea382cf
Marcilia <guilgerm@gmail.com>
2021-04-11 14:28:27 -0300

    preparação do projeto para iniciar testes de integração

---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NerdStore.WebApp.MVC;
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace NerdStore.WebApp.Tests.Config
+{
+    [CollectionDefinition(nameof(IntegrationWebTestsFixtureCollection))]
+    public class IntegrationWebTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>> { }
+    
+    [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>> { }
+
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly LojaAppFactory<TStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixture()
+        {
+            var clientOptions = new WebApplicationFactoryClientOptions
+            {
+                //TODO: criar opções
+            };
+            Factory = new LojaAppFactory<TStartup>();
+            Client = Factory.CreateClient(clientOptions); //já olha direto para o servidor da aplição
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Factory.Dispose();
+        }
+    }
+}
---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -0,0 +1,31 @@
+﻿using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NerdStore.WebApp.Tests
+{
+    //Para injetar automaticamente as dependencias
+    [Collection(nameof(IntegrationWebTestsFixtureCollection))]
+    public class UsuarioTests
+    {
+        private readonly IntegrationTestsFixture<StartupWebTests> _testsFixture;
+        public UsuarioTests(IntegrationTestsFixture<StartupWebTests> testsFixture)
+        {
+            //a fixture não é recriada a cada teste! ela mantém o estado.
+            _testsFixture = testsFixture;
+        }
+
+        [Fact(DisplayName = "Realizar cadastro com sucesso")]
+        [Trait("Categoria", "Integração Web - usuário")]
+        public async Task Usuario_RealizarCadastro_DeveExecutarComSucesso()
+        {
+            var initialResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Register");
+            initialResponse.EnsureSuccessStatusCode();
+        }
+
+    }
+}
